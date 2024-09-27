574d7f021dc13ea085fd15a6c7b2cd4edcefcae5
Hee Seo Kim <hisounni@ewhain.net>
2024-09-27 15:35:23 +0900

    9/27

--- JourneySentinel/app.py
+++ JourneySentinel/app.py
@@ -1,29 +1,19 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# 도난 시도 데이터 저장 (메모리 내 리스트에 저장)
-detection_data = []
 
-# index.html 렌더링을 위한 경로 설정
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/warning', methods=['POST'])
+@app.route('/warning', methods=['POST'])  # POST 방식으로 /warning 경로 설정
 def warning():
     message = request.form.get('message')  # ESP32에서 전송하는 message 값
     timestamp = request.form.get('timestamp')  # ESP32에서 전송하는 timestamp 값
-    location = request.form.get('location')  # ESP32에서 전송하는 location 값
 
-    if message and timestamp and location:
-        # 수신된 데이터를 detection_data 리스트에 추가
-        detection_data.append({"message": message, "timestamp": timestamp, "location": location})
-        print(f"Received message: {message} at {timestamp} in {location}")
+    if message and timestamp:
+        print(f"Received message: {message} at {timestamp}")
         return jsonify({'status': 'success', 'message': 'Data received'}), 200
     else:
         return jsonify({'status': 'error', 'message': 'Missing data'}), 400
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')  # 로컬 네트워크에서 서버 실행
+    app.run(host='0.0.0.0', port=5000)  # 로컬 네트워크에서 서버 실행
