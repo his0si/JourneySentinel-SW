ecfa2994490dc402b967ffce8e411c5059be474f
Hee Seo Kim <hisounni@ewhain.net>
2024-09-27 14:23:22 +0900

    9/27

--- JourneySentinel/static/app.js
+++ JourneySentinel/static/app.js
@@ -1,35 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-
     fetch('/get-theft-detection-data')
         .then(response => response.json())
         .then(data => {
             const detectionList = document.getElementById('detection-list');
 
-            // Clear existing content
-            detectionList.innerHTML = '';
-
-            // Display fetched data
             data.forEach(item => {
                 const detectionItem = document.createElement('div');
                 detectionItem.classList.add('detection-item');
 
-                // Timestamp
-                const timestamp = document.createElement('p');
-                timestamp.classList.add('timestamp');
-                timestamp.textContent = `시간: ${item.timestamp}`;
+                const date = document.createElement('p');
+                date.classList.add('date');
+                date.textContent = item.date;
 
-                // Location
                 const location = document.createElement('p');
                 location.classList.add('location');
-                location.textContent = `장소: ${item.location}`;
+                location.textContent = item.location;
 
-                // Append timestamp and location
-                detectionItem.appendChild(timestamp);
+                detectionItem.appendChild(date);
                 detectionItem.appendChild(location);
                 detectionList.appendChild(detectionItem);
             });
         })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
+        .catch(error => console.error('Error fetching data:', error));
 });
